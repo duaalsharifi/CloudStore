c484034f017367f7b0581dbc75720983172ed7bd
duaaa <duaa.alsharifi02@hotmail.com>
2026-04-30 10:48:09 +0200

    add Product.java

---
+++ src/main/java/se/jensen/duaa/cloudstore/model/Product.java
@@ -0,0 +1,56 @@
+package se.jensen.duaa.cloudstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
